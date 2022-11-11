60b183d80376759659f39ab5f64f572034616b52
Evan Rosson <git@erosson.org>
2022-11-11 17:03:15 -0500

    prettier

--- src/DebugScreen.tsx
+++ src/DebugScreen.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  ScrollView,
-  View,
-  Text,
-  Switch,
-  Platform,
-  Button,
-} from "react-native";
+import { ScrollView, View, Text, Switch, Platform, Button } from "react-native";
 import AsyncStorage from "@react-native-community/async-storage";
 import Constants from "expo-constants";
 import * as Feature from "./feature";
