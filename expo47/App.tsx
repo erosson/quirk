dc84ae73aefd49cd936c44cf1a2a08219a26e725
Evan Rosson <git@erosson.org>
2022-11-16 21:00:23 -0500

    finished screen, hacky

--- expo47/App.tsx
+++ expo47/App.tsx
@@ -5,7 +5,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler"
 import * as Feature from "./src/feature";
 // import CBTListScreen from "./src/CBTListScreen";
 import CBTFormScreen from "./src/form/FormScreen";
-// import FinishedThoughtScreen from "./src/form/FinishedThoughtScreen";
+import FinishedThoughtScreen from "./src/form/FinishedThoughtScreen";
 import ExplanationScreen from "./src/ExplanationScreen";
 // import SettingScreen from "./src/SettingsScreen";
 import OnboardingScreen from "./src/OnboardingScreen";
@@ -31,7 +31,7 @@ export function App() {
           <Stack.Screen name={Screen.EXPLANATION} component={ExplanationScreen} />
           {/* <Stack.Screen name={Screen.SETTING} component={SettingScreen} /> */}
           {/* <Stack.Screen name={Screen.INIT} component={InitScreen} /> */}
-          {/* <Stack.Screen name={Screen.FINISHED_THOUGHT} component={FinishedThoughtScreen} /> */}
+          <Stack.Screen name={Screen.FINISHED_THOUGHT} component={FinishedThoughtScreen} />
           {/* <Stack.Screen name={Screen.LOCK} component={LockScreen} /> */}
           <Stack.Screen name={Screen.DEBUG} component={DebugScreen} options={{headerShown: true, title: ""}} />
         </Stack.Navigator>
