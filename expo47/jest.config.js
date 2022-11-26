a9fbbd24a284f871c995e45f343eebfda43748c6
Evan Rosson <git@erosson.org>
2022-11-26 02:04:41 -0500

    externalize json-decoding library to @erosson/json-encode-decode

--- expo47/jest.config.js
+++ expo47/jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     // (some?) transformed modules must be listed explicitly. this line is from the
     // docs below, with a few extra modules specific to this project added at the end:
     // https://docs.expo.io/guides/testing-with-jest/#jest-configuration
-    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|sentry-expo|native-base|@sentry/.*|react-pose-core|animated-pose|i18n-js)",
+    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|sentry-expo|native-base|@sentry/.*|react-pose-core|animated-pose|i18n-js|@erosson/json-encode-decode)",
   ],
   moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx"],
   "setupFiles": ["./src/jestSetup.js"],
